78f1463bd9493b0044421f17bccc15d6933f6ae2
unknown <vladancupric@gmail.com>
2022-01-20 13:50:50 +0100

    New commits

--- task02/project01/src/App.js
+++ task02/project01/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Zdravo from "./Zdravo";
+// import Zdravo from "./Zdravo";
 import Objava from "./Objava";
 import './App.css';
 
 function App() {
   
   return (
-    <div className="App objava">
-      <Zdravo/>
-      <Objava/>
-     
+    <div className="app">
+      {/* <Zdravo/> */}
+      <Objava ime="Ranko Stajic" objava= "Golman" brojPregleda="1"/>
+      <Objava ime="Stefan Kostov" objava="Desni Bek" brojPregleda ="2" />
+      <Objava ime="Dragan Ikanovic" objava= "Levi Bek" brojPregleda = "3"/>
+      <Objava ime="Bogdan Palezevic" objava="Desni Half" brojPregleda = "4"/>
+  
     </div>
   );
 }
--- task02/project01/src/Objava.js
+++ task02/project01/src/Objava.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 
-function Objava(){
+function Objava(props){
     return (
-        <div>
-                Broj pregleda
+        <div className="objava">
+        <h4>{props.ime}</h4>
+        <p>{props.objava}</p>
+        <h4>Broj pregleda objave</h4>
         </div>
     )
 }
