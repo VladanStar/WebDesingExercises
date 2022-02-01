417e1b309829324d12c3f0016f288ca4c35b5c28
unknown <vladancupric@gmail.com>
2022-02-01 19:44:00 +0100

    New commits

--- task03/informationsystem/src/components/StudentList.js
+++ task03/informationsystem/src/components/StudentList.js
@@ -1,32 +1,30 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Student from "./Student";
-import './StudentList.css';
-
+import "./StudentList.css";
 
 const StudentList = () => {
-  const[students, setStudents] = useState([])
-  
-  useEffect(()=>{
-    fetch('./students.json')
-    .then(
-      data => data.json() )
-      .then(data => console.log(data)
-      )
-    .then(
-      students => setStudents(students) )
-  },[]);
- 
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("./students.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => console.log(data))
+      .then(students => setStudents(students));
+  }, []);
+
   return (
-   <div className="StudentList">
-{
-
-students && students.map(student =>
-          <Student key={student.jmbg}
-                   student={student} />
-        )
-}
-
-  </div>)
+    <div className="StudentList">
+      {students &&
+        students.map((student) => (
+          <Student key={student.jmbg} student={student} />
+        ))}
+    </div>
+  );
 };
 
 export default StudentList;
