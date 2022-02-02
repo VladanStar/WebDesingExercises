1831c65352f9ade3b4b0c85c2ff54d35059eff00
unknown <vladancupric@gmail.com>
2022-02-02 11:35:54 +0100

    New commits

--- task03/informationsystem/src/App.js
+++ task03/informationsystem/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import StudentList from './components/StudentList';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import StudentForm from './components/StudentForm';
 import Header from './components/Header';
+import Login from './components/Login';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loadingUser, setLoadingUser] =useState(true);
+
+  function onLogin() {
+    setIsLoggedIn(true)
+  }
+  function onLogut() {
+    setIsLoggedIn(false);
+  }
+
+
+
+
+  if (!isLoggedIn) {
+    return (
+      <div className="App">
+        <Login onLogin={onLogin}/>
+      </div>
+    )
+  }
+
+
   
   return (
     <div className="App">
--- task03/informationsystem/src/components/Header.js
+++ task03/informationsystem/src/components/Header.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css";
 
-const Header = () => {
+const Header = (props) => {
+
+    function logout() {
+        fetch("/logout").then(() => {
+          props.onLogout();
+        });
+      }
+    
+    
   return (
   <header className='Header'>
       <Link to='/students'>Students</Link>
       <Link to="/students/new"> Add Student</Link>
+      <button onClick={logout}>Logout</button>
 
   </header>
   )
