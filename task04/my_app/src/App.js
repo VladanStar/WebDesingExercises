a72c0b74e7c270287678b9dfa613e41aea9c52e0
unknown <vladancupric@gmail.com>
2022-02-06 20:25:43 +0100

    New commits

--- task04/my_app/src/App.js
+++ task04/my_app/src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import Header from './components/Header';
+import { AddContact } from './components/AddContact';
+import { ContactList } from './components/ContactList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+   <Header />
+   <AddContact />
+   <ContactList />
     </div>
   );
 }
--- task04/my_app/src/components/Header.js
+++ task04/my_app/src/components/Header.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Header = () => {
   return (
-  <div>
+
+
+  <div className='ui fixed menu'>
+      <div className=' ui container center'>
+          <h2>Contact Manager</h2>
+
+      </div>
 
 
   </div>
