776a313c3f8e87117e95d6c6d8861c21155c3644
unknown <vladancupric@gmail.com>
2022-02-06 09:44:00 +0100

    New commits

---
+++ task04/my_app/src/components/ContactCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ContactCard = () => {
+  return (
+  <div>
+      
+  </div>
+  )
+};
---
+++ task04/my_app/src/components/ContactList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ContactList = () => {
+  return ( 
+  <div>
+      
+  </div>
+  )
+};
