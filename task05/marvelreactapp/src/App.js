aae7400bc0dcf010566c5ee0f8d36e61c3e4b228
unknown <vladancupric@gmail.com>
2022-02-08 14:55:24 +0100

    New commits

---
+++ task05/marvelreactapp/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './App.css';
+import Header from "./component/Header/Header";
+
+function App() {
+
+  //const key ="38eaa2f8c31badac01a5ef5daa2d3c2f";
+  return (
+    <div className="App container">
+      <Header />
+    
+    </div>
+  );
+}
+
+export default App;
