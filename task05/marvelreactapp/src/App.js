54887cd50f5d0578d2c8566e4955fb529bd2d26f
unknown <vladancupric@gmail.com>
2022-02-08 16:05:42 +0100

    new commits

--- task05/marvelreactapp/src/App.js
+++ task05/marvelreactapp/src/App.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import './App.css';
 import Header from "./component/Header/Header";
+import axios from "axios";
 
 function App() {
 
-  //const key ="38eaa2f8c31badac01a5ef5daa2d3c2f";
+   const hash ="bd0722d5750b6362d5ba0212ca36726b";
+
+  const[items, setItems] = useState([]);
+  const[isLoading, setLoading] = useState(true);
+
+  //axios + `key kopitan iz developer.marvel.com authenitication for server side application copy http://...`
+
+  useEffect(()=>{
+     const fetch = async()=>{
+      const result = await axios(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=344d40df0c8cc373141c1dc321fae9cf&hash=bd0722d5750b6362d5ba0212ca36726b`)
+      console.log(result.data);
+      setItems(result.data);
+      setLoading(false)
+     }
+
+     fetch()
+  },[])
+
+ 
   return (
     <div className="App container">
       <Header />
