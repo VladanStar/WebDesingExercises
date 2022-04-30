b2d163179ee726c1a815fee38780d121921da6bc
VladanStar <vladancupric@gmail.com>
2022-04-30 21:38:00 +0200

    New commits

--- task09/script.js
+++ task09/script.js
@@ -1,35 +1,36 @@
-
 /*
 Response Header
 */
 const mobileMenu = () => {
-    console.log("Test");
-    let menu = document.querySelector('.header ul');
-    let btn = document.querySelector('.header button');
+  console.log("Test");
+  let menu = document.querySelector(".header ul");
+  let btn = document.querySelector(".header button");
 
-    console.log(menu);
-    if (btn.innerText === "MENU") {
-        console.log("MENU write");
-        menu.style.display = 'block';
-        btn.innerHTML = "CLOSE"
-    }
-    else {
-        console.log("CLOSE write");
-        menu.style.display = 'none';
+  console.log(menu);
+  if (btn.innerText === "MENU") {
+    console.log("MENU write");
+    menu.style.display = "block";
+    btn.innerHTML = "CLOSE";
+  } else {
+    console.log("CLOSE write");
+    menu.style.display = "none";
 
-        btn.innerHTML = "MENU"
-    }
-   
-}
+    btn.innerHTML = "MENU";
+  }
+};
 /*
 Galerija
 */
 let rightBtn = document.querySelector("#right-btn");
-let leftBtn = document.querySelector('#left-btn');
-
-rightBtn.addEventListener('click', () => {
-    console.log("Right")
-})
+let leftBtn = document.querySelector("#left-btn");
+let pictures = document.querySelectorAll(".slider-images img");
+let imgNum = 0;
+rightBtn.addEventListener("click", () => {
+  console.log("Right");
+  console.log(pictures);
+  pictures[0].style.display = "none";
+  pictures[1].style.display = "block";
+});
 leftBtn.addEventListener("click", () => {
-    console.log("Left");
-})
+  console.log("Left");
+});
